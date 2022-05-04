7699d2b13d1ca5bd451af51f75b56d68c7b1d39c
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2022-05-04 14:42:59 -0400

    added to README.md and added some endpoints

--- README.md
+++ README.md
@@ -5,8 +5,9 @@
 - Familiarize oneself with git commands and git/github interactions 
 - Create README.md file in the desired local folder 
 - Stage, Commit and Push changes to an empty github repo 
-- Add a .js file in the folder and begin step 2
+- Add a index.js file in the folder and install npm and express into the project directory 
 
 ##Step 2
 - Build an API using Node.js as a backend runtime engine
 - Test the API's endpoints with Postman
+- Postman is used to debug the API throughout the completion of the URL-Shortener
--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const app = require('express')();//initializes the app 
+const express = require('express');
+const app =express();//initializes the app 
 const PORT = 8080;
 
+app.use(express.json())
+
+//first endpoint... response status is 200 meaning everything is okay, then info is sent 
+app.get('/tshirt', (req, res) => {
+    res.status(200).send(
+        {
+        tshirt: 'xy', 
+        size: 'large'
+        }
+    )
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo)
+    {
+        res.status(418).send({message: 'We need a logo!'})
+    }
+
+    res.send({tshirt: `X with your ${logo} and ID of ${id}`})
+});
+
+
 app.listen(
-    PORT, () => console.log('its alive on http://localhost:${PORT}')
+    PORT, () => console.log(`its alive on http://localhost:${PORT}`)
 )//runs the API on a server defined by the port 
+ 
