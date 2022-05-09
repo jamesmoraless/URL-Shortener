8b0774176d8a7daeab5e23d1f742ce7164d720b3
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2022-05-09 02:49:30 -0400

    added HTML forms and base code for link shortening

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
 const express = require('express');
-const app =express();//initializes the app 
+const app = express();//initializes the app 
 const PORT = 8080;
 
-app.use(express.json())
+app.use(express.json())//middleware that makes the request into a json object
+app.use(express.static("public")) //this middleware gets the static files in public folder and executes this before anything below
 
 //first endpoint... response status is 200 meaning everything is okay, then info is sent 
-app.get('/tshirt', (req, res) => {
-    res.status(200).send(
-        {
-        tshirt: 'xy', 
-        size: 'large'
-        }
-    )
-});
+app.post('/longUrl', (req, res) => {
+    //const { link } = req.params;
 
-app.post('/tshirt/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
+    const { link } = req.body;//instead of body, we must get this link from a text field OR in the html file, we use formmethod so longUrl="insertedlink" will be the request
+    const rand = Math.floor(10000 + (90000 - 10000) * Math.random());
 
-    if (!logo)
+    if (!link)
     {
-        res.status(418).send({message: 'We need a logo!'})
+        res.status(418).send({message: 'We need a link!'})
     }
 
-    res.send({tshirt: `X with your ${logo} and ID of ${id}`})
+    res.status(200).send(`myShort.url/${rand}`)//figure out how to point this to the inserted link variable
 });
-
-//I need to add a html text box where you can insert a URL and that could be used as the request
-//Then, the request (url) can be stored in a newLink = "shorturl.at/randomeChar() + randomeChar() + randomChar() + randomeChar()"
-// app.patch('/here I need the link to be updated/shortened', (req, res) => {
-//     const { link } = req.params;
-
-//     if (!link)
-//     {
-//         res.status(418).send({message: 'We need a link!'})
-//     }
-
-//     res.send({shortUrl: `${link}`})
-// });
-
 
 app.listen(
     PORT, () => console.log(`its alive on http://localhost:${PORT}`)
-)//runs the API on a server defined by the port 
+)//runs the API on a server defined by the port (port location, function)
  
