49dd0c68eccf014861d9faea43fd3e80bd9cf2ca
jamesmoraless <71559183+jamesmoraless@users.noreply.github.com>
2022-05-22 17:04:21 -0400

    modified files for review

--- service.js
+++ service.js
@@ -4,24 +4,30 @@ class DatabaseService {
   _db;
   constructor() {
     this._db = new sqlite3.Database(
-      "./linkBank.db",
+      "./myLinks.db",
       sqlite3.OPEN_READWRITE,
       (err) => {
         if (err) return console.error(err.message);
         console.log("DB connection succesful");
       }
     ); //connects to the database
-
-    //this._db.run("CREATE TABLE oLinks(id, originLink)");//created my table
   }
-  intoDB(id, link) {
-    const sqlInto = "INSERT INTO oLinks(id, originLink) VALUES (?,?)"; //reusable query to insert a row into the table with an id and origin link value
+  generateID(length) {
+    return parseInt(
+      Math.ceil(Math.random() * Date.now())
+        .toPrecision(length)
+        .toString()
+        .replace(".", "")
+    );
+  }
+  insertLink(id, link) {
+    const sqlInto = "INSERT INTO myLinks(id, originLink) VALUES (?,?)"; //reusable query to insert a row into the table with an id and origin link value
     this._db.run(sqlInto, [id, link], (err) => {
       if (err) console.error(err.message);
     }); //adds a row with the inputted data
   }
-  printAll() {
-    const sqlSelect = "SELECT * FROM oLinks"; //reusable query to select all rows from the table in order to visualize changes
+  readAllLinks() {
+    const sqlSelect = "SELECT * FROM myLinks"; //reusable query to select all rows from the table in order to visualize changes
 
     this._db.all(sqlSelect, [], (err, rows) => {
       if (err) console.error(err.message);
@@ -30,10 +36,9 @@ class DatabaseService {
       });
     }); //prints out all the rows in the database
   }
-
   getOriginLink(id) {
     return new Promise((resolve, reject) => {
-      const sqlSelect1 = `SELECT originLink FROM oLinks WHERE id = ${id}`; //retrieves the original link from the provided id
+      const sqlSelect1 = `SELECT originLink FROM myLinks WHERE id = ${id}`; //retrieves the original link from the provided id
       this._db.serialize(() => {
         this._db.all(sqlSelect1, (err, rows) => {
           if (err) reject(err);
@@ -42,8 +47,8 @@ class DatabaseService {
       });
     });
   }
-  restartDB() {
-    this._db.run("DELETE FROM oLinks"); //deleted my table at the end of running the server
+  clearLinksList() {
+    this._db.run("DELETE FROM myLinks"); //deleted my table at the end of running the server
   }
 }
 
